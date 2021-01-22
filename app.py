fddbddb1d4e234662637e174b9309f476b59e31d
adam181189 <adam181189@gmail.com>
2021-01-22 15:37:59 +0000

    made sure renders of login/reg show flashes correctly. Username shows up on Corner of Navbar

--- app.py
+++ app.py
@@ -33,24 +33,21 @@ def music_collection():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # check if username exists in db
-        existing_user = mongo.db.users.find_one(
+        users = mongo.db.users
+        existing_user = users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            # ensure hashed password matches user input
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                         session["user"] = request.form.get("username").lower()
                         return redirect(url_for(
                             "home", username=session["user"]))
             else:
-                # invalid password match
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
-            # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
     return render_template("login.html")
@@ -59,7 +56,8 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        existing_user = mongo.db.users.find_one(
+        users = mongo.db.users
+        existing_user = users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
@@ -74,8 +72,8 @@ def register():
         mongo.db.users.insert_one(register)
 
         session["userLog"] = request.form.get("username").lower()
-        flash("You are registered!")
-        return redirect(url_for("home", username=session["userLog"]))
+        flash("You are registered! Please log in")
+        return redirect(url_for("login", username=session["userLog"]))
     
     return render_template("register.html")
 
