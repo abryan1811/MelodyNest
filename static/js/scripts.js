a04a27fba0b2c0703f7912c6ab051c51a93ee68f
adam181189 <adam181189@gmail.com>
2021-01-27 21:05:14 +0000

    allow cards in music to play, pause and stop the music uploaded to MongoDB

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,56 @@
+/* Variables */
+var music = document.getElementById('music'); // id for audio element
+var duration = music.duration; // Duration of audio clip, calculated here for embedding purposes
+var playButton = document.getElementById('playButton'); // play button
+var stopButton = document.getElementById('stopButton'); // stop button
 
+
+
+// Gets audio file duration
+music.addEventListener("canplaythrough", function () {
+    duration = music.duration;
+}, false);
+
+//Play Button
+
+// play button work when clicked
+playButton.addEventListener("click", play);
+
+//Play and Pause
+
+function play() {
+    // start music
+    if (music.paused) {
+        music.play();
+        // remove play, add pause
+        playButton.className = "";
+        playButton.className = "fas fa-pause";
+    } else { // pause music
+        music.pause();
+        // remove pause, add play
+        playButton.className = "";
+        playButton.className = "fas fa-play";
+    }
+}
+
+// Stop Button
+
+// stop button works when clicked
+stopButton.addEventListener("click", stop);
+
+// stop and reset
+
+function stop() {
+    if (music.pause){
+        playButton.className = "";
+        playButton.className = "fas fa-play"
+    }
+    music.currentTime = 0
+    music.pause();
+}
+
+// getPosition
+// Returns elements left position relative to top-left of viewport
+function getPosition(el) {
+    return el.getBoundingClientRect().left;
+}
