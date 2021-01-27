a079264d0a4d4826be485e75032a578b39a2926d
adam181189 <adam181189@gmail.com>
2021-01-27 22:52:40 +0000

    set up share file so music files can be added to mongo db

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,56 +1,57 @@
 /* Variables */
-var music = document.getElementById('music'); // id for audio element
-var duration = music.duration; // Duration of audio clip, calculated here for embedding purposes
-var playButton = document.getElementById('playButton'); // play button
-var stopButton = document.getElementById('stopButton'); // stop button
+//var music = document.getElementById('music'); // id for audio element
+//var duration = music.duration; // Duration of audio clip, calculated here for embedding purposes
 
+var playButtons = $('.playButton'); // play buttons
+var stopButtons = $('.stopButton'); // stop buttons
 
+playButtons.on("click", play);
+stopButtons.on("click", stop);
 
 // Gets audio file duration
-music.addEventListener("canplaythrough", function () {
-    duration = music.duration;
-}, false);
 
 //Play Button
-
-// play button work when clicked
-playButton.addEventListener("click", play);
 
 //Play and Pause
 
 function play() {
+    console.log("PLAY");
+
+    $(".music").each(function() {
+        this.pause();
+    })
+
+    var music = $("#music" + $(this).data("id"))[0];
+    var playButton = $("#play" + $(this).data("id"))[0];
+    var stopButton = $("#play" + $(this).data("id"))[0];
+
     // start music
     if (music.paused) {
         music.play();
         // remove play, add pause
-        playButton.className = "";
-        playButton.className = "fas fa-pause";
+        playButton.className = "playButton fas fa-pause";
     } else { // pause music
         music.pause();
         // remove pause, add play
-        playButton.className = "";
-        playButton.className = "fas fa-play";
+        playButton.className = "playButton fas fa-play";
     }
 }
 
 // Stop Button
 
-// stop button works when clicked
-stopButton.addEventListener("click", stop);
-
 // stop and reset
 
 function stop() {
+    console.log("STOP");
+
+    var music = $("#music" + $(this).data("id"))[0];
+    var playButton = $("#play" + $(this).data("id"))[0];
+    var stopButton = $("#play" + $(this).data("id"))[0];
+
     if (music.pause){
         playButton.className = "";
-        playButton.className = "fas fa-play"
+        playButton.className = "playButton fas fa-play"
     }
     music.currentTime = 0
     music.pause();
-}
-
-// getPosition
-// Returns elements left position relative to top-left of viewport
-function getPosition(el) {
-    return el.getBoundingClientRect().left;
 }
