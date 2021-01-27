5382ff28af6ac19aeb42e5f073c4253a55bd54e4
adam181189 <adam181189@gmail.com>
2021-01-27 23:07:31 +0000

    reverted a commit and used jquery to access the specific data Ids to play the specific track belonging to the added track on share.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,3 @@
-/* Variables */
-//var music = document.getElementById('music'); // id for audio element
-//var duration = music.duration; // Duration of audio clip, calculated here for embedding purposes
 
 var playButtons = $('.playButton'); // play buttons
 var stopButtons = $('.stopButton'); // stop buttons
@@ -8,47 +5,36 @@ var stopButtons = $('.stopButton'); // stop buttons
 playButtons.on("click", play);
 stopButtons.on("click", stop);
 
-// Gets audio file duration
-
-//Play Button
-
-//Play and Pause
-
 function play() {
-    console.log("PLAY");
 
-    $(".music").each(function() {
+    $(".music").each(function () {
         this.pause();
+        this.currentTime = 0
     })
 
     var music = $("#music" + $(this).data("id"))[0];
     var playButton = $("#play" + $(this).data("id"))[0];
     var stopButton = $("#play" + $(this).data("id"))[0];
 
-    // start music
-    if (music.paused) {
-        music.play();
-        // remove play, add pause
-        playButton.className = "playButton fas fa-pause";
-    } else { // pause music
+    if (music.currentTime > 1) {
         music.pause();
         // remove pause, add play
         playButton.className = "playButton fas fa-play";
     }
+    else { // pause music
+        music.play();      
+        
+    }
 }
 
-// Stop Button
-
-// stop and reset
+//Stop Button
 
 function stop() {
-    console.log("STOP");
-
     var music = $("#music" + $(this).data("id"))[0];
     var playButton = $("#play" + $(this).data("id"))[0];
     var stopButton = $("#play" + $(this).data("id"))[0];
 
-    if (music.pause){
+    if (music.stop){
         playButton.className = "";
         playButton.className = "playButton fas fa-play"
     }
