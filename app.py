f43f264bcc6ac8da8c038ee7aef61f1c588ab80c
adam181189 <adam181189@gmail.com>
2021-03-12 18:43:00 +0000

    heroku not deploying so trying with debug being set back to True

--- app.py
+++ app.py
@@ -745,4 +745,4 @@ def delete_self(profile_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
