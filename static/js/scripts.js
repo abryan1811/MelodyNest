103debdf47f029c2f6b1cb2f5fde5053b1822726
adam181189 <adam181189@gmail.com>
2021-03-12 15:39:03 +0000

    tidied up code to ensure it was fully readable

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,85 +7,82 @@ stopButtons.on("click", stop);
 
 function play() {
 
-    $(".music").each(function () {
-        this.pause();
-        this.currentTime = 0;
-    });
+  $(".music").each(function() {
+    this.pause();
+    this.currentTime = 0;
+  });
 
-    var music = $("#music" + $(this).data("id"))[0];
-    var playButton = $("#play" + $(this).data("id"))[0];
+  var music = $("#music" + $(this).data("id"))[0];
+  var playButton = $("#play" + $(this).data("id"))[0];
 
-    if (music.currentTime > 1) {
-        music.pause();
-        // remove pause, add play
-        playButton.className = "playButton fas fa-play";
-    }
-    else { // pause music
-        music.play();
+  if (music.currentTime > 1) {
+    music.pause();
+    // remove pause, add play
+    playButton.className = "playButton fas fa-play";
+  } else { // pause music
+    music.play();
 
-    }
+  }
 }
 
 //Stop Button
 
 function stop() {
-    var music = $("#music" + $(this).data("id"))[0];
-    var playButton = $("#play" + $(this).data("id"))[0];
+  var music = $("#music" + $(this).data("id"))[0];
+  var playButton = $("#play" + $(this).data("id"))[0];
 
-    if (music.stop) {
-        playButton.className = "";
-        playButton.className = "playButton fas fa-play";
-    }
-    music.currentTime = 0;
-    music.pause();
+  if (music.stop) {
+    playButton.className = "";
+    playButton.className = "playButton fas fa-play";
+  }
+  music.currentTime = 0;
+  music.pause();
 }
 
 /* Add Genre to the share dropdown */
 function newGenre() {
-    if ($("#inputGenre").val() == "addNew") {
-        $("#newGenreText").removeClass("d-none");
-    }
-    else {
-        $("#newGenreText").addClass("d-none");
-    }
+  if ($("#inputGenre").val() == "addNew") {
+    $("#newGenreText").removeClass("d-none");
+  } else {
+    $("#newGenreText").addClass("d-none");
+  }
 }
 
 /* Add Instrument to the share dropdown */
 function newInstrument() {
-    if ($("#inputInstrument").val() == "addNew") {
-        $("#newInstrumentText").removeClass("d-none");
-    }
-    else {
-        $("#newInstrumentText").addClass("d-none");
-    }
+  if ($("#inputInstrument").val() == "addNew") {
+    $("#newInstrumentText").removeClass("d-none");
+  } else {
+    $("#newInstrumentText").addClass("d-none");
+  }
 }
 
 /* Add option to see the password when typed in */
 //code located https://www.w3schools.com/howto/howto_js_toggle_password.asp
 function showPassword() {
-    var x = document.getElementById("passwordLogin");
-    if (x.type === "password") {
-        x.type = "text";
-    } else {
-        x.type = "password";
-    }
+  var x = document.getElementById("passwordLogin");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
 
 /* If review is over 10 words, have it shrink down until More is clicked to read the rest. */
-$(".reviewText").each(function (e, v) {
-    $(v).data("full-text", $(v).html());
-    if ($(v).html().split(' ').length > 10) {
-        $(v).html($(v).html().split(' ').slice(0, 10).join(' ') + " ... " + '<a class="expandReview">More</a>');
-    }
+$(".reviewText").each(function(e, v) {
+  $(v).data("full-text", $(v).html());
+  if ($(v).html().split(' ').length > 10) {
+    $(v).html($(v).html().split(' ').slice(0, 10).join(' ') + " ... " + '<a class="expandReview">More</a>');
+  }
 
-    $(this).click(function () {
-        $(v).html($(v).data("full-text"));
-    });
+  $(this).click(function() {
+    $(v).html($(v).data("full-text"));
+  });
 });
 
 /* ask user if they are sure they want to delete an item */
 function certainDelete() {
-    if (!confirm("Are you sure you wish to delete this?")) {
-        event.preventDefault();        
-    }
+  if (!confirm("Are you sure you wish to delete this?")) {
+    event.preventDefault();
+  }
 }
